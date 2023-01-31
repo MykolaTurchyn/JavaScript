9d142c0ed59d48282804bf04fbc0f55cd2e8945a
MykolaTurchyn <tyrchnmikigt@gmail.com>
2023-01-31 21:16:06 +0200

    2022

---
+++ plan_2022/main.js
@@ -0,0 +1,115 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+// let arr = ["hello", null, undefined, 0, 'okten', true, false, 10];
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+// console.log(arr[5]);
+// console.log(arr[6]);
+// console.log(arr[7]);
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+// let book1 = {
+//     title: "something good",
+//     pageCount: 300,
+//     genre: "trialer",
+// };
+//
+// let book2 = {
+//     title: "hello everybody",
+//     pageCount: 534,
+//     genre: "comedy"
+// };
+//
+// let book3 = {
+//     title: "10 years later",
+//     pageCount: 315,
+//     genre: "fantasy"
+// }
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+// let book1 = {
+//     title: "something good",
+//     pageCount: 300,
+//     genre: "trialer",
+//     authors: ["Mihael", "Kantus"]
+// };
+//
+// let book2 = {
+//     title: "hello everybody",
+//     pageCount: 534,
+//     genre: "comedy",
+//     authors: ["Bober", "Kokos"]
+// };
+//
+// let book3 = {
+//     title: "10 years later",
+//     pageCount: 315,
+//     genre: "fantasy",
+//     authors: ["Apolon", "Mufonic"]
+// }
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+// let users = [
+//     {
+//         name: "anna",
+//         username: "boberey",
+//         password: this.name + this.username
+//     },
+//     {
+//         name: "petya",
+//         username: "vovchar",
+//         password: this.name + this.username
+//     },
+//     {
+//         name: "sergiy",
+//         username: "petrov",
+//         password: this.name + this.username
+//     },
+//     {
+//         name: "anton",
+//         username: "karychan",
+//         password: this.name + this.username
+//     },
+//     {
+//         name: "sasha",
+//         username: "sheva",
+//         password: this.name + this.username
+//     },
+//     {
+//         name: "kostya",
+//         username: "pupkin",
+//         password: this.name + this.username
+//     },
+//     {
+//         name: "danylo",
+//         username: "sorokov",
+//         password: this.name + this.username
+//     },
+//     {
+//         name: "mykyta",
+//         username: "voltov",
+//         password: this.name + this.username
+//     },
+//     {
+//         name: "karina",
+//         username: "dudok",
+//         password: this.name + this.username
+//     },
+//     {
+//         name: "sofia",
+//         username: "kurova",
+//         password: this.name + this.username
+//     }
+// ];
+// console.log(users[0].password);
+// console.log(users[1].password);
+// console.log(users[2].password);
+// console.log(users[3].password);
+// console.log(users[4].password);
+// console.log(users[5].password);
+// console.log(users[6].password);
+// console.log(users[7].password);
+// console.log(users[8].password);
+// console.log(users[9].password);
